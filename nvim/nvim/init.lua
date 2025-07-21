839067d5320b691a7f79edeed204919d4e088ee5
iamsg97 <iamsg97@outlook.com>
2025-07-21 18:08:02 +0000

    feat: updated neovim config with new theme (visual studio theme)

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -778,25 +778,43 @@ require("lazy").setup({
 		},
 	},
 
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
+	{
+		"Mofiqul/vscode.nvim",
+		name = "vscode",
 		config = function()
-			---@diagnostic disable-next-line: missing-fields
-			require("catppuccin").setup({
-				flavour = "frappe",
-				no_italic = true,
-			})
+			local c = require("vscode.colors").get_colors()
+			require("vscode").setup({
+				-- Alternatively set style in setup
+				-- style = 'light'
 
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("catppuccin")
+				-- Enable transparent background
+				transparent = true,
+
+				-- Enable italic comment
+				italic_comments = true,
+
+				-- Underline `@markup.link.*` variants
+				underline_links = true,
+
+				-- Disable nvim-tree background color
+				disable_nvimtree_bg = true,
+
+				-- Apply theme colors to terminal
+				terminal_colors = true,
+
+				-- Override colors (see ./lua/vscode/colors.lua)
+				color_overrides = {
+					vscLineNumber = "#FFFFFF",
+				},
+
+				-- Override highlight groups (see ./lua/vscode/theme.lua)
+				group_overrides = {
+					-- this supports the same val table as vim.api.nvim_set_hl
+					-- use colors from this colorscheme by requiring vscode.colors!
+					Cursor = { fg = c.vscDarkBlue, bg = c.vscLightGreen, bold = true },
+				},
+			})
+			vim.cmd("colorscheme vscode")
 		end,
 	},
 
